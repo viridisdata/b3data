f5652079f13f7bd5ff9328daa108d13213dd4405
Daniel Komesu <danielkomesu@gmail.com>
2022-07-09 21:05:18 -0300

    📒 Update
    Rename package to b3-cotacaohistorica-fetcher
    Add script to fetch raw data files within a date range
    Add date validation

---
+++ b3_cotacaohistorica_fetcher.py
@@ -0,0 +1,157 @@
+
+__version__ = "0.1.0"
+
+import datetime as dt
+from pathlib import Path
+
+import httpx
+from tqdm import tqdm
+
+DateTuple = tuple[int]
+
+
+class InvalidDate(Exception):
+    pass
+
+
+def carnaval_date(year: int) -> tuple[dt.date]:
+    # https://www.vivaolinux.com.br/script/Calcular-a-data-do-Carnaval-e-da-Pascoa
+    x = 24
+    y = 5
+    a = year % 19
+    b = year % 4
+    c = year % 7
+    d = ( 19 * a + x ) % 30
+    e = ( 2 * b + 4 * c + 6 * d + y ) % 7
+    if ( (d + e ) > 9 ):
+        day = d + e - 9
+        pascoa = dt.date(year, 4, day)
+        month = 4
+        data1 = dt.date(year, month, day )
+        # O carnaval sera a subtração de 47 dias da data da pascoa
+        carnaval = dt.date.fromordinal(data1.toordinal() - 47)
+    else:
+        day = d + e + 22
+        pascoa = dt.date(year, 3, day)
+        month = 3
+        data1 = dt.date(year, month, day)
+        carnaval = dt.date.fromordinal(data1.toordinal() - 47)
+    return carnaval, pascoa
+
+
+def valid_year(year: int) -> bool:
+    return year > 1985 and year <= dt.date.today().year
+
+
+def valid_month(year: int, month: int) -> bool:
+    today = dt.date.today()
+    if not valid_year(year):
+        return False
+    if month not in range(1, 13):
+        return False
+    if month == today.month and year == today.year:
+        return False
+    if year == today.year:
+        return month < today.month
+    else:
+        if year == today.year - 1:
+            return month >= today.month
+    return False
+
+
+def valid_day(datetuple: DateTuple) -> bool:
+    year, month, day = datetuple
+    return dt.date(year, month, day).year == dt.date.today().year
+
+
+def valid_date(datetuple: DateTuple) -> bool:
+    year, month, day = datetuple
+    if day is None:
+        return True
+    date = dt.date(year, month, day)
+    weekday = date.weekday()
+    workday = weekday != 5 and weekday != 6
+    brazillian_holidays = (
+        dt.date(year, 1, 1),    # 1 de janeiro (Ano novo)
+        dt.date(year, 4, 21),   # 21 de abril (Tiradentes)
+        dt.date(year, 5, 1),    # 1 de maio (Dia do Trabalhador)
+        dt.date(year, 9, 7),    # 7 de setembro (Dia da Independência do Brasil)
+        dt.date(year, 10, 12),  # 12 de outubro (Nossa Senhora Aparecida)
+        dt.date(year, 11, 2),   # 2 de novembro (Dia do Finados)
+        dt.date(year, 11, 15),  # 15 de novembro (Proclamação da República)
+        dt.date(year, 12, 25),  # 25 de dezembro (Natal)
+    )
+    not_holiday = not any(date == holiday for holiday in brazillian_holidays)
+    return workday and not_holiday
+
+
+def get_url(datetuple: DateTuple) -> str:
+    BASE_URL = "https://bvmf.bmfbovespa.com.br/InstDados/SerHist/COTAHIST_"
+    year, month, day = datetuple
+    if month is None:
+        return f"{BASE_URL}A{year}.zip"
+    elif day is None:
+        return f"{BASE_URL}M{month:02}{year}.zip"
+    else:
+        return f"{BASE_URL}D{day:02}{month:02}{year}.zip"
+
+
+def get_filename(datetuple: DateTuple, modified: dt.datetime = None) -> str:
+    year, month, day = datetuple
+    if month is None:
+        return f"b3-cotacaohistorica_{year}_{modified:%Y%m%d}.zip"
+    elif day is None:
+        return f"b3-cotacaohistorica_{year}{month:02}_{modified:%Y%m%d}.zip"
+    else:
+        return f"b3-cotacaohistorica_{year}{month:02}{day:02}_{modified:%Y%m%d}.zip"
+
+
+def get_dest_filepath(
+    datadir: Path,
+    datetuple: DateTuple,
+    modified: dt.date = None,
+) -> Path:
+    year, _, _ = datetuple
+    filename = get_filename(datetuple, modified)
+    return datadir / f"{year}" / filename
+
+
+def fetch_data_file(datadir: Path, datetuple: DateTuple, client: httpx.Client = None, blocksize: int = 8192) -> None:
+    if not valid_date(datetuple):
+        raise InvalidDate(f"Invalid date {datetuple}")
+    year, month, day = datetuple
+    url = get_url(datetuple)
+    if client is not None:
+        r = client.get(url)
+    else:
+        r = httpx.get(url, verify=False)
+
+    if r.status_code != 200:
+        print(f"Error fetching {url}: {r.status_code}")
+        r.raise_for_status()
+    elif r.headers.get("content-type") != "application/x-zip-compressed":
+        raise Exception(f"Error fetching {url}: {r.headers.get('content-type')}")
+
+    file_size = int(r.headers.get("content-length", 0))
+    modified = r.headers.get("last-modified")
+    if modified:
+        modified = dt.datetime.strptime(modified, "%a, %d %b %Y %H:%M:%S %Z")
+
+    # Destination file path
+    dest_filepath = get_dest_filepath(datadir, datetuple, modified)
+    if dest_filepath.exists():
+        return
+    elif not dest_filepath.parent.exists():
+        dest_filepath.parent.mkdir(parents=True)
+
+    progress = tqdm(
+        total=file_size,
+        unit="B",
+        unit_scale=True,
+        desc=f"{dest_filepath.name}",
+    )
+    with open(dest_filepath, "wb") as f:
+        for chunk in r.iter_bytes(blocksize):
+            f.write(chunk)
+            progress.update(len(chunk))
+    progress.close()
--- fetch.py
+++ fetch.py
@@ -1,12 +1,64 @@
 import argparse
-import pathlib
-from b3_ch_fetcher import fetcher
+import datetime as dt
+import re
+from pathlib import Path
+
+import httpx
+
+from b3_cotacaohistorica_fetcher import DateTuple, fetch_data_file, InvalidDate
+
+HTTP_HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/102.0.5005.149 Safari/537.36 "
+        "Vivaldi/5.3.2679.68"
+    ),
+}
+
+
+def parse_dates(date_range: str) -> list[DateTuple]:
+    dates = []
+    start, end = sorted(date_range.split(":"))
+
+    yearly_match = re.match(r"\d{4}:\d{4}", date_range)
+    monthly_match = re.match(r"\d{4}-\d{2}:\d{4}-\d{2}", date_range)
+    daily_match = re.match(r"\d{4}-\d{2}-\d{2}:\d{4}-\d{2}-\d{2}", date_range)
+
+    if yearly_match:
+        start, end = int(start), int(end)
+        dates.extend(
+            [
+                (year, None, None)
+                for year in range(start, end + 1)
+            ]
+        )
+    elif monthly_match:
+        start_year, start_month = start.split("-")
+        end_year, end_month = end.split("-")
+        start_year, start_month = int(start_year), int(start_month)
+        end_year, end_month = int(end_year), int(end_month)
+        for year in range(start_year, end_year + 1):
+            for month in range(1, 13):
+                if (month < start_month and year == start_year):
+                    continue
+                elif (month > end_month and year == end_year):
+                    continue
+                dates.append((year, month, None))
+    elif daily_match:
+        start_date = dt.datetime.strptime(start, "%Y-%m-%d").date()
+        end_date = dt.datetime.strptime(end, "%Y-%m-%d").date()
+        date = start_date
+        while date <= end_date:
+            dates.append((date.year, date.month, date.day))
+            date += dt.timedelta(days=1)
+    return dates
 
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--ouput", dest="output", type=pathlib.Path)
-    parser.add_argument("years", nargs="+", type=int)
+    parser.add_argument("-o", "--ouput", dest="output", type=Path)
+    parser.add_argument("dates", help="start:end")
     return parser
 
 
@@ -14,9 +66,17 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
     output = args.output
-    years = args.years
-    for year in years:
-        fetcher.fetch_year(year, dest=dest)
+    dates = parse_dates(args.dates)
+
+    client = httpx.Client(headers=HTTP_HEADERS, verify=False)
+
+    for date in dates:
+        try:
+            fetch_data_file(datadir=output, datetuple=date, client=client)
+        except InvalidDate:
+            print(f"Invalid date: {date}")
+        except httpx.HTTPStatusError:
+            print(f"HTTP error: {date}")
 
 
 if __name__ == "__main__":
