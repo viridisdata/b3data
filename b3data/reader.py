f880e85bb55e3645f73c98ff2f5acc3040d8d7d3
Daniel Komesu <danielkomesu@gmail.com>
2023-01-07 16:32:32 -0300

    Add rename option

--- b3data/reader.py
+++ b3data/reader.py
@@ -24,38 +24,11 @@ def _remove_text_extra_spaces(s: pd.Series) -> pd.Series:
     return s.replace(" +", " ", regex=True)
 
 
-def read_registro(filepath: Path) -> pd.DataFrame:
-    layout = {
-        "tipreg": [1, 2],
-        "data": [3, 10],
-        "bdi_id": [11, 12],
-        "simbolo": [13, 24],
-        "tipo_mercado_id": [25, 27],
-        "nome_resumido": [28, 39],
-        "especificacao": [40, 49],
-        "prazo": [50, 52],
-        "simbolo_moeda": [53, 56],
-        "preco_abertura": [57, 69],
-        "preco_maximo": [70, 82],
-        "preco_minimo": [83, 95],
-        "preco_medio": [96, 108],
-        "preco_ultimo_negocio": [109, 121],
-        "preco_melhor_oferta_compra": [122, 134],
-        "preco_melhor_oferta_venda": [135, 147],
-        "quantidade_negocios": [148, 152],
-        "quantidade_titulos_negociados": [153, 170],
-        "volume_negociado": [171, 188],
-        "preco_exercicio": [189, 201],
-        "indice_correcao_id": [202, 202],
-        "data_vencimento": [203, 210],
-        "fator_cotacao": [211, 217],
-        "preco_exercicio_pontos": [218, 230],
-        "codisi": [231, 242],
-        "dismes": [243, 245],
-    }
-    NAMES = list(layout.keys())
-    WIDTHS = [y - x + 1 for x, y in layout.values()]
-    d = pd.read_fwf(
+def read_data(filepath: Path, rename_columns: bool = False) -> pd.DataFrame:
+    registro01 = read_registro01()
+    NAMES = registro01["NOME DO CAMPO"]
+    WIDTHS = registro01["POS. FINAL"] - registro01["POS. INIC."] + 1
+    df = pd.read_fwf(
         filepath,
         compression="zip",
         widths=WIDTHS,
@@ -63,15 +36,41 @@ def read_registro(filepath: Path) -> pd.DataFrame:
         skiprows=1,
         skipfooter=1,
         encoding="latin-1",
-        parse_dates=["data", "data_vencimento"],
+        parse_dates=["DATA", "DATVEN"],
     )
-    d = d.assign(
-        data_vencimento=_parse_data_vencimento(d["data_vencimento"]),
-        prazo=d["prazo"].apply(_to_float),
-        especificacao=_remove_text_extra_spaces(d["especificacao"]),
+    df = df.assign(
+        data_vencimento=_parse_data_vencimento(df["DATVEN"]),
+        prazo=df["PRAZOT"].apply(_to_float),
+        especificacao=_remove_text_extra_spaces(df["ESPECI"]),
     )
-    d = d.drop(columns=["tipreg"])
-    return d
+    df = df.drop(columns=["TIPREG"])
+    if rename_columns:
+        names = {
+            a: b
+            for _, a, b in registro01[
+                ["NOME DO CAMPO", "nome_normalizado"]
+            ].itertuples()
+        }
+        df = df.rename(columns=names)
+    return df
+
+
+def read_registro00():
+    with resources.path("b3data.auxiliary_tables", "registro00.csv") as filepath:
+        df = pd.read_csv(filepath)
+    return df
+
+
+def read_registro01():
+    with resources.path("b3data.auxiliary_tables", "registro01.csv") as filepath:
+        df = pd.read_csv(filepath)
+    return df
+
+
+def read_registro99():
+    with resources.path("b3data.auxiliary_tables", "registro99.csv") as filepath:
+        df = pd.read_csv(filepath)
+    return df
 
 
 def read_codbdi():
