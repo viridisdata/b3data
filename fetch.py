dd9f5f997cc84a11c328484ade045d40d02d0f65
Daniel Komesu <danielkomesu@gmail.com>
2023-01-07 16:34:32 -0300

    Add script fetch.py

---
+++ fetch.py
@@ -0,0 +1,33 @@
+import argparse
+from pathlib import Path
+
+from b3data.dates import parse_dates
+from b3data.fetcher import fetch_dates
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-o",
+        "--ouput",
+        dest="output",
+        type=Path,
+        default=Path("data"),
+    )
+    parser.add_argument("dates", help="start:end")
+    args = parser.parse_args()
+    return {
+        "output": args.output,
+        "dates": parse_dates(args.dates),
+    }
+
+
+def main():
+    args = get_args()
+    dates = args["dates"]
+    output = args["output"]
+    fetch_dates(dates, output)
+
+
+if __name__ == "__main__":
+    main()
